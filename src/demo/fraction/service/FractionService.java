824000e158813939d4cb6e2c7ac556f2231a21ec
softgen007 <softgen007@gmail.com>
2019-10-12 13:38:27 -0700

    added fraction service

---
+++ src/demo/fraction/service/FractionService.java
@@ -0,0 +1,35 @@
+package demo.fraction.service;
+
+import demo.fraction.model.FractionNumber;
+
+public class FractionService {
+
+	
+	public FractionNumber multiplyFractions(FractionNumber op1, FractionNumber op2) {
+		int num = op1.getImproperNumerator() * op2.getImproperNumerator();
+		int den = op1.getDenominator() * op2.getDenominator();
+		
+		return new FractionNumber(num + "/" + den);
+	}
+	
+	public FractionNumber divideFractions(FractionNumber op1, FractionNumber op2) {
+		int num = op1.getImproperNumerator() * op2.getDenominator();
+		int den = op1.getDenominator() * op2.getImproperNumerator();
+		
+		return new FractionNumber(num + "/" + den);
+	}
+	
+	public FractionNumber addFractions(FractionNumber op1, FractionNumber op2) {
+		int num = (op1.getImproperNumerator() * op2.getDenominator()) + (op2.getImproperNumerator() * op1.getDenominator());
+		int den = op1.getDenominator() * op2.getDenominator();
+		
+		return new FractionNumber(num + "/" + den);
+	}
+	
+	public FractionNumber subtractFractions(FractionNumber op1, FractionNumber op2) {
+		int num = (op1.getImproperNumerator() * op2.getDenominator()) - (op2.getImproperNumerator() * op1.getDenominator());
+		int den = op1.getDenominator() * op2.getDenominator();
+		
+		return new FractionNumber(num + "/" + den);
+	}
+}
