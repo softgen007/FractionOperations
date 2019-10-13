77909d086f8d2fcc45495b0e0103fa37788441d7
softgen007 <softgen007@gmail.com>
2019-10-12 17:51:51 -0700

    updated fraction app main class to read input from command line

--- src/demo/fraction/FractionApp.java
+++ src/demo/fraction/FractionApp.java
@@ -1,22 +1,30 @@
 package demo.fraction;
 
+import java.util.Scanner;
+
+import demo.fraction.controller.FractionController;
 import demo.fraction.model.FractionNumber;
-import demo.fraction.util.FractionOperations;
+import demo.fraction.service.FractionService;
 
 public class FractionApp {
 
 	public static void main(String[] args) {
-		String operand1 = "-3_1/2";
-		String operand2 = "3/2";
-		
-		System.out.println(new FractionOperations().multiplyFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
+		//Sample input 5_2/3 * 3/2
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Input: ");
+		String str = sc.nextLine();
+		System.out.println("Str: " + str);
+		sc.close();
+		System.out.println("Str: " + str);
+		//System.out.println(new FractionService().multiplyFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
 
-		System.out.println(new FractionOperations().divideFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
+		//System.out.println(new FractionService().divideFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
 
-		System.out.println(new FractionOperations().addFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
+		//System.out.println(new FractionService().addFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
 
-		System.out.println(new FractionOperations().subtractFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
+		//System.out.println(new FractionService().subtractFractions(new FractionNumber(operand1), new FractionNumber(operand2)));
 
+		System.out.println(new FractionController().perfromOperation(str));
 	}
 
 }
